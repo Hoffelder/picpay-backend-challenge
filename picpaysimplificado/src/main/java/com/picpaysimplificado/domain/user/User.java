372603814be55c21707f24bde685c9856bd80c12
Vinicius Hoffelder <vinicius.hoffelder1@gmail.com>
2024-03-26 16:45:28 -0300

    feat: Updating User Entity

--- picpaysimplificado/src/main/java/com/picpaysimplificado/domain/user/User.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.picpaysimplificado.domain.user;
 
+import com.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -40,5 +41,20 @@ public class User {
 
     public BigDecimal setBalance(BigDecimal subtract) {
         return this.balance;
+    }
+
+    public String getEmail() {
+
+        return getEmail();
+    }
+
+    public User(UserDTO data){
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+        this.email = data.email();
+
     }
 }
